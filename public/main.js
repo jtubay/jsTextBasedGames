4d21a1986c32d634135e7a69fb382870b07f2ca5
Jean Tubay <jean.tubay.92@gmail.com>
2020-07-19 23:57:39 -0400

    updating files

--- public/main.js
+++ public/main.js
@@ -1,9 +1,16 @@
+const rooms = {
+    room0: "<p>You are in a dark and cold place. You see a light to the <b>North</b> and hearing running water to the <b>West</b></p>"
+}
+console.log(rooms.room0)
+
 $(document).ready(() => {
-    const input = $('#user-input').val();
+    $('#game-text').append(rooms.room0)
     $(document).keypress(key => {
 
-        if(key.which === 13) {
-            console.log('hello')
+        if(key.which === 13 && $('#user-input').is(':focus')) {
+            const userInput = $('#user-input').val();
+
+            console.log(userInput)
         }
     })
 
